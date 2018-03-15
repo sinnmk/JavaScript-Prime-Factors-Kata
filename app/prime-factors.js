cfe5b05d9d8dd8e27bbdd8909070939d07b60e4d
Michelle Sinn <sinn.mk@gmail.com>
2018-03-15 14:18:19 -0500

    prime factors kata in javascript

--- app/prime-factors.js
+++ app/prime-factors.js
@@ -1,7 +1,16 @@
 class PrimeFactors {
-
-  getPrime(n) {
-    return n;
+  
+  getPrime(n){ 
+    var result = []
+    while(n > 1){
+    for(var divisor = 2; divisor <=n; divisor++){
+        while(n % divisor == 0){
+          result.push(divisor)
+          n/=divisor
+        }   
+      }
+    }
+    return result 
   }
 }
 
--- test/prime-factors.js
+++ test/prime-factors.js
@@ -1,9 +1,26 @@
-var expect = require("chai").expect;
-var primes = require("../app/prime-factors");
+let expect = require("chai").expect;
+let primes = require("../app/prime-factors");
 
-describe("Prime Pactors", function() {
-  it("returns the passed number", function() {
-    expect(new primes().getPrime(3)).equal(3)
-  });
+describe("Prime Factors", function() {
 
+   it("Prime Factors of 1", function() {
+     expect(new primes().getPrime(1)).to.deep.equal([])
+   });
+
+   it("Prime Factors of 2", function() {
+     expect(new primes().getPrime(2)).to.deep.equal([2])
+   });
+
+   it("Prime Factors of 4", function() {
+     expect(new primes().getPrime(4)).to.deep.equal([2,2])
+   });
+
+   it("Prime Factors of 8", function() {
+     expect(new primes().getPrime(8)).to.deep.equal([2,2,2])
+   });
+
+   it("Prime Factors of 9", function() {
+     expect(new primes().getPrime(9)).to.deep.equal([3,3])
+   });
 });
+
